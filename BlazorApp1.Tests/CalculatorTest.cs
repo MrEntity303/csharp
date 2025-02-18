999ed2ca41ba926c62783b777bb689d6bb25f54f
MrEntity303 <patryk.bialowas@studenti.unicam.it>
2025-02-18 14:46:32 +0100

    first commit

---
+++ BlazorApp1.Tests/CalculatorTest.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+using BlazorApp1.Services;
+namespace BlazorApp1.Tests;
+
+public class CalculatorTests
+{
+    [Fact]
+    public void Somma_DueNumeri_RitornaSommaCorretta()
+    {
+        var calc = new Calculator();
+        var risultato = calc.Somma(2, 3);
+        Assert.Equal(5, risultato);
+    }
+}
---
+++ BlazorApp1/Services/Calculator.cs
@@ -0,0 +1,9 @@
+namespace BlazorApp1.Services;
+
+public class Calculator
+{
+    public int Somma(int a, int b)
+    {
+        return a + b;
+    }
+}
